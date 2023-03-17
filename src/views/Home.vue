8b74859655151ebb2218c809c25a14c60a2960cd
Joanne Affolter <joanne.affolter@epfl.ch>
2023-03-17 21:59:40 +0100

    Eleknik

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <v-card class="mb-6">
       <v-card-title class = "text-h1">ElecNIK ⚡</v-card-title>
-      <v-img src="src/assets/images/doigt-dhonneur.gif"></v-img>
+      <v-img src="@/assets/images/doigt-dhonneur.gif"></v-img>
 
 
     </v-card>
