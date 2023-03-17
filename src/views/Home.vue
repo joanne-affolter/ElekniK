117593c058c2ef24cd30ef5617e48f97820194f6
Joanne Affolter <joanne.affolter@epfl.ch>
2023-03-17 22:23:48 +0100

    Eleknik

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,7 +56,7 @@ export default {
         Authorization: 'Bearer ' + access_token,
         'Content-Type': 'application/json',
       }
-      const query = 'Furck you' // your song query here
+      const query = 'Fuck you' // your song query here
       const type = 'track'
       const url = `https://api.spotify.com/v1/search?q=${query}&type=${type}`
 
